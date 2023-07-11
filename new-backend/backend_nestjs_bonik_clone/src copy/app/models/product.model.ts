1b3d6c59b8dce1c79319c0aa425f27e8e5b6178f
Minh Hieu <minhhieuhuynh140@gmail.com>
2023-07-11 23:56:04 +0700

    remove source copy

--- new-backend/backend_nestjs_bonik_clone/src copy/app/models/product.model.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-  readonly productId: number;
-  readonly categoryId: string;
-  readonly name: string;
-  price: number;
-  description: string;
-  rating: number;
-}
--- new-backend/backend_nestjs_bonik_clone/src copy/config/database.config.ts
+++
@@ -1,7 +0,0 @@
-export const databaseConfig = {
-  host: 'localhost',
-  port: 3306,
-  username: 'admin',
-  password: 'admin',
-  database: 'bonik-clone',
-};
